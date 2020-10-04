3ffb259980a47ec3db540091975976afdc7f07c4
Kim Säppi <sappikim@gmail.com>
2020-10-04 15:45:25 +0300

    Restructuring

--- src/server/urls.py
+++ src/server/urls.py
@@ -1,4 +1,4 @@
-"""laundry URL Configuration
+"""server URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.1/topics/http/urls/
