268eee567f0456d08f7f9abe7f5bb4eedba820f6
Kim Säppi <sappikim@gmail.com>
2020-10-19 19:43:30 +0300

    Apartment selection works

--- frontend/src/components/Inputs.js
+++ frontend/src/components/Inputs.js
@@ -2,15 +2,15 @@ import React, { useEffect, useState } from 'react';
 
 import * as config from '../config.json';
 
-const StairInput = () => {
+const nullValue = 'PlaceholderThatShouldNotExist';
+
+const StairInput = ({ stair, setStair, setApartment }) => {
   const stairs = Object.keys(config.apartments);
-  const nullValue = 'PlaceholderThatShouldNotExist'
-  const [value, setValue] = useState(nullValue);
 
   useEffect(() => {
     const oldStair = localStorage.getItem('stair');
     if (oldStair)
-      setValue(oldStair);
+      setStair(oldStair);
   }, []);
 
   const stairOptions = stairs.map((stairName, index) =>
@@ -19,22 +19,67 @@ const StairInput = () => {
 
   const handleChange = event => {
     const newStair = event.target.options[event.target.selectedIndex].text;
-    setValue(event.target.options[event.target.selectedIndex].text);
+    setStair(event.target.options[event.target.selectedIndex].text);
     localStorage.setItem('stair', newStair);
+    setApartment(nullValue);
   };
 
   return (
-    <select onChange={handleChange} value={value}>
+    <select onChange={handleChange} value={stair}>
       <option disabled value={nullValue}>Stair</option>
       {stairOptions}
     </select>
   );
 };
 
+const ApartmentInput = ({ apartment, setApartment, stair }) => {
+  const apartments = Array.isArray(config.apartments[stair]) ?
+    config.apartments[stair] : [];
+
+  useEffect(() => {
+    const oldApartment = localStorage.getItem('apartment');
+    if (oldApartment)
+      setApartment(oldApartment);
+  }, []);
+
+  const apartmentOptions = apartments.map((apartmentName, index) =>
+    <option key={index}>{apartmentName}</option>
+  );
+
+  const handleChange = event => {
+    const newApartment = event.target.options[event.target.selectedIndex].text;
+    setApartment(event.target.options[event.target.selectedIndex].text);
+    localStorage.setItem('apartment', newApartment);
+  };
+
+  return (
+    <select onChange={handleChange} value={apartment}>
+      <option disabled value={nullValue}>Apartment</option>
+      {apartmentOptions}
+    </select>
+  );
+};
+
 const Inputs = () => {
+  const [stair, setStair] = useState(nullValue);
+  const [apartment, setApartment] = useState(nullValue);
+
+  useEffect(() => {
+    const oldApartment = localStorage.getItem('apartment');
+    if (oldApartment)
+      setApartment(oldApartment)
+  }, []);
+
   return (
     <form>
-      <StairInput/>
+      <StairInput
+        stair={stair} setStair={setStair}
+        setApartment={setApartment}
+      />
+      <ApartmentInput
+        apartment={apartment} setApartment={setApartment}
+        stair={stair}
+      />
     </form>
   );
 };
