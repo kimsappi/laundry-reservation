cfb87c4348e8b85cb576fc9092abc68884f07cc7
Kim Säppi <sappikim@gmail.com>
2020-11-03 20:30:01 +0200

    Fixed tests

--- backend/laundry/tests.py
+++ backend/laundry/tests.py
@@ -1,10 +1,11 @@
 import urllib
 import json
+import datetime
 
 from django.test import TestCase, Client
 
-from .views import Reservations
-from .models import Machine, Reservation
+from .views import Reservations, getCurrentDisplayTime
+from .models import Machine, Reservation, Owner
 
 # Create your tests here.
 
@@ -12,14 +13,17 @@ c = Client()
 
 MACHINE_NAME = 'TestMachine'
 CANCEL_CODE = 'TestCancelCode'
-OWNER = 'A42'
+OWNER = 'A 42'
 RESERVATIONS_ENDPOINT = '/api/reservations'
 EXAMPLE_TEST_CASE = {
-  'time': '2018-06-29 08:15:27.243860',
+  'date': '2020-11-03',
+  'time': 15,
   'machine': MACHINE_NAME,
   'cancelCode': CANCEL_CODE,
   'owner': OWNER
 }
+# Tests will fail if run at n:59:59...
+CURRENT_TIME = getCurrentDisplayTime()
 
 def readContent(res):
   return json.loads(res.content.decode('utf-8'))
@@ -27,24 +31,36 @@ def readContent(res):
 def getReservations():
   return c.get(RESERVATIONS_ENDPOINT)
 
-def putReservation():
-  return c.put(RESERVATIONS_ENDPOINT, json.dumps([EXAMPLE_TEST_CASE]).encode('utf-8'))
+def putReservation(owner, cancelCode, reservations, cancellations):
+  return c.post(
+    RESERVATIONS_ENDPOINT,
+    data=json.dumps({
+      'owner': owner, 'cancelCode': cancelCode,
+      'new': reservations, 'cancel': cancellations
+    }),
+    content_type='application/json'
+  )
 
 class CreateSlotTestCase(TestCase):
   def setUp(self):
     Machine.objects.create(name=MACHINE_NAME)
+    Owner.objects.create(name=OWNER)
 
   def testGet(self):
     res = getReservations()
     self.assertEqual(res.status_code, 200)
     body = readContent(res)
     db = list(Reservation.objects.values('time', 'machine', 'owner'))
-    self.assertDictEqual(body, {'reservations': db})
+    self.assertDictEqual(body, {
+      'date': CURRENT_TIME['date'],
+      'reservations': db,
+      'time': CURRENT_TIME['time']
+    })
   
   def testPut(self):
-    res = putReservation()
-    self.assertEqual(res.status_code, 201)
+    res = putReservation(EXAMPLE_TEST_CASE['owner'], EXAMPLE_TEST_CASE['cancelCode'], [{'time': EXAMPLE_TEST_CASE['time'], 'date': EXAMPLE_TEST_CASE['date'], 'machine': EXAMPLE_TEST_CASE['machine']}], [])
+    self.assertEqual(res.status_code, 200)
     body = readContent(res)
-    self.assertDictEqual(body, {'success': [EXAMPLE_TEST_CASE['time']], 'failure': []})
-    db = Reservation.objects.get(time=EXAMPLE_TEST_CASE['time'])
-    self.assertEqual(db.owner, EXAMPLE_TEST_CASE['owner'])
+    self.assertDictEqual(body['success'][0], {'date': EXAMPLE_TEST_CASE['date'], 'time': EXAMPLE_TEST_CASE['time'], 'machine': MACHINE_NAME, 'type': 'reservation'})
+    db = Reservation.objects.get(date=EXAMPLE_TEST_CASE['date'], time=EXAMPLE_TEST_CASE['time'])
+    self.assertEqual(str(db.owner), EXAMPLE_TEST_CASE['owner'])
